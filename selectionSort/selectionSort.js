fb75a3bb640d62b37f8808e75b9eb05bd9610c34
Travis <trajamsmith@gmail.com>
2018-02-24 20:05:17 -0600

    Add selectionSort algorithm

--- selectionSort/selectionSort.js
+++ selectionSort/selectionSort.js
@@ -8,5 +8,27 @@ function swapArrayElements (array, firstIndex, secondIndex) {
 swapArrayElements([0,1,2,3,4], 3, 4);
 
 function subArrayMinIndex (array, startIndex) {
-
+    let first = array[startIndex];
+    let smallest = first;
+    let indexSmallest = startIndex;
+    for ( let i = startIndex + 1; i < array.length; i++) {
+        if ( array[i] < smallest) {
+            smallest = array[i];
+            indexSmallest = i;
+        }
+    }
+    return indexSmallest;
 }
+
+subArrayMinIndex([4,5,17,7,8,12],0);
+
+function selectionSort (array) {
+    let smallestIndex;
+    for ( let i = 0; i < array.length; i++){
+        smallestIndex = subArrayMinIndex(array, i);
+        swapArrayElements(array, i, smallestIndex);
+    }
+    console.log(array);
+}
+
+selectionSort([1,7,6,5,12,4]);
